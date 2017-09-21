8e2d8afb067c996ca42f5ffa49c9cb2efb02ff5c
Jed Iquin <jedikin@gmail.com>
2017-09-20 20:03:11 -0700

    bot replies to dms now

--- bot.py
+++ bot.py
@@ -7,6 +7,10 @@ CONFIG = 'config.cfg'
 BOT_SCRIPTS = 'scripts.json'
 AT_BOT = None #TODO find another way to do this, it would be great to create a bot class
 
+PUBLIC = 1
+PRIVATE = 2
+DM = 3
+
 #load the bot settings
 config = configparser.ConfigParser()
 config.read(CONFIG)
@@ -19,15 +23,44 @@ scripts = json.load(script_file)
 #create the bot client using credentials
 bot = slackclient.SlackClient(config['BOT']['token'])
 
+def get_channel_type(channel):
+    
+    if channel[0] == 'D':
+        return DM
+    if channel[0] == 'C':
+        return PUBLIC
+    if channel[0] == 'G':
+        return PRIVATE
+
+    # return {
+    #     'D' : DM,
+    #     'C' : PUBLIC,
+    #     'G' : PRIVATE
+    # }[channel[0]]
+
+    return None 
+
+
 #create a simple reply to 'hi' when talking to bot
 #reply should include user's name
 def parse_slack_output(slack_output_list):
     
     if slack_output_list and len(slack_output_list) > 0:
         for output in slack_output_list:
-            if output and 'text' in output and AT_BOT in output['text']:
-                return output['text'].strip(AT_BOT).strip().lower(), output['channel'], output['user']
-
+            
+            if output and 'text' in output:
+                #check if the channel is DM or public/private
+                ch_type = get_channel_type(output['channel'].strip())
+                
+                print('channel type is', ch_type)
+                if ch_type == DM:
+                    return output['text'], output['channel'], output['user']
+                if ch_type == PUBLIC or ch_type == PRIVATE:
+                    #if it is in public/private 
+                    #check if the message is directed to the bot
+                    if AT_BOT in output['text']:
+                        return output['text'].strip(AT_BOT).strip().lower(), output['channel'], output['user']
+                
     return None, None, None
 
 def handle_command(command, channel, user):
